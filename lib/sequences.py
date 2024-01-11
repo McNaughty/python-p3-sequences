303b2708aa8bdf9fa6bdac57e86039fd2ced7dd1
Winnie <wimacoj@gmail.com>
2024-01-11 15:24:23 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # print(list([]))
+    if length < 1:
+        indexes = list(range(length))
+        print(indexes)
+    elif length == 1:
+        indexes = list(range(length))
+        # print(indexes)
+        print(indexes)
+    elif length == 2:
+        indexes = list(range(length))
+        # print(indexes)
+        print(indexes)
+    else:
+        # fibonacci always start with 0 and 1
+        indexes=[0,1]
+
+        for i in range(2, length):
+            indexes.append(indexes[-1] + indexes[-2])
+        print(indexes)
+
+
+    
+
